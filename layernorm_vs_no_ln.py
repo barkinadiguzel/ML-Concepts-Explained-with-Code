2ccf0b10aa04bbab07bc2803d370acb609ace6a2
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-09-26 23:21:08 +0300

    Update layernorm_vs_no_ln.py
    This script compares the effect of using Layer Normalization in a simple neural network. It shows how adding LayerNorm can impact the training accuracy and stability.

--- layernorm_vs_no_ln.py
+++ layernorm_vs_no_ln.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-
+ 
 # ------------------------
 # 1️⃣ Dataset and Transform
 # ------------------------
